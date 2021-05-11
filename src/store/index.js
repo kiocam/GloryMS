9745d9bc2dc2d16274032019785f2e3c1a3ed1a0
kiocam <jasoncameron@Jasons-MacBook-Pro.local>
2021-05-11 10:58:44 -0400

    vuex state plugin added

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+
 
 import mutations from './mutations'
 import actions from './actions'
@@ -11,6 +13,7 @@ const initialState = () => {
   return { user: null, errors: null}
 }
 export default new Vuex.Store({
+  plugins: [createPersistedState],
   state: initialState(),
   mutations: mutations,
   actions: actions,
